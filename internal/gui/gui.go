757cff990bcd108040bfdc35e7ef1c27cf083bf3
Cosmin Tupangiu <teodor-cosmin.tupangiu@akka.eu>
2020-02-18 07:34:29 +0100

    fix typo

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
-	"github.com/tupyy/lazylogger/internal/nternal/log"
+	"github.com/tupyy/lazylogger/internal/log"
 )
 
 const keyOne = rune('1')
